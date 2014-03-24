92d928ed2b4d45ae54eb55419f4630abcbcb6902
Hrvoje Šimić <shime.ferovac@gmail.com>
2014-03-24 09:32:58 +0100

    introduce DataImporter

--- import_data.rb
+++ import_data.rb
@@ -11,14 +11,7 @@ require "./models/route_type"
 
 require "./page"
 require "./chunk"
-
-Mongoid.load!("mongoid.yml", :development)
-
-Line.destroy_all
-DayType.destroy_all
-Direction.destroy_all
-Departure.destroy_all
-RouteType.destroy_all
+require "./data_importer"
 
 POSITION = {
   :line_name => 3,
@@ -29,42 +22,12 @@ POSITION = {
 URL = "http://www.samoborcek.hr/linija.php?id=18" 
 
 page = Page.new(URL)
-
-line_name  = page.get_title(POSITION[:line_name]).first
-day_types  = page.get_title(POSITION[:day_types])
-directions = page.get_title(POSITION[:directions])
-types      = page.get_title(POSITION[:route_types])
-
-line       = Line.create(title: line_name)
-day_types  = day_types.map {|t| DayType.create(title: t)}
-directions = directions.map {|t| Direction.create(title: t)}
-types      =  types.map {|t| RouteType.create(title: t)}
-types      << RouteType.create(title: "direct")
-
-def populate_chunk_pairs(position, line, day_type, page)
-  [[position, Direction.zagreb],
-   [position + 7, Direction.samobor]].each do |pair|
-    Chunk.fetch(
-      position: pair.first,
-      line: line,
-      direction: pair.last,
-      day_type: day_type,
-      page: page
-    ) 
-  end
-end
-
-populate_chunk_pairs(7,
-                     Line.first,
-                     DayType.radni,
-                     page)
-
-populate_chunk_pairs(23,
-                     Line.first,
-                     DayType.subota,
-                     page)
-
-populate_chunk_pairs(38,
-                     Line.first,
-                     DayType.nedjelja,
-                     page)
+importer = DataImporter.new(
+  :line_definition => {
+    :radni => 7,
+    :subota => 23,
+    :nedjelja => 38 
+  },
+  :page => page
+)
+importer.import_all
