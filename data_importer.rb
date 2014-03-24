d8a8ea6096591c2f1525fafcccfc534cd9e2afb1
Hrvoje Šimić <shime.ferovac@gmail.com>
2014-03-24 09:39:37 +0100

    get rid of POSITION in favor of schema

--- data_importer.rb
+++ data_importer.rb
@@ -1,8 +1,8 @@
 class DataImporter
   def initialize(opts)
-    @line_definition = opts[:line_definition] ||
+    @page_schema = opts[:page_schema] ||
       raise(ArgumentError.new("can't find
-        :line_definition in the arguments"))
+        :page_schema in the arguments"))
 
     @page = opts[:page] ||
       raise(ArgumentError.new("can't find :page in
@@ -18,11 +18,11 @@ class DataImporter
     import_directions
     import_route_types
 
-    @line_definition.each do |day_type, position|
+    @page_schema[:departure_positions].each do |day_type, position|
       import_chunk_pairs(position,
                          Line.first,
                          DayType.send(day_type),
-                         page)
+                         @page)
     end
   end
 
@@ -40,10 +40,6 @@ class DataImporter
       RouteType.destroy_all
     end
 
-    def page
-      @page
-    end
-
     def import_chunk_pairs(position, line, day_type, page)
       [[position, Direction.zagreb],
        [position + 7, Direction.samobor]].each do |pair|
@@ -59,22 +55,22 @@ class DataImporter
 
     def import_line
       line_name = 
-        page.get_title(POSITION[:line_name]).first
+        @page.get_title(@page_schema[:line_name]).first
       Line.create(title: line_name)
     end
 
     def import_day_types
-      day_types  = page.get_title(POSITION[:day_types])
+      day_types  = @page.get_title(@page_schema[:day_types])
       day_types.each {|t| DayType.create(title: t)}
     end
 
     def import_directions
-      directions = page.get_title(POSITION[:directions])
+      directions = @page.get_title(@page_schema[:directions])
       directions.each {|t| Direction.create(title: t)}
     end
 
     def import_route_types
-      types      = page.get_title(POSITION[:route_types])
+      types      = @page.get_title(@page_schema[:route_types])
 
       types.each {|t| RouteType.create(title: t)}
       RouteType.create(title: "direct")
--- import_data.rb
+++ import_data.rb
@@ -13,21 +13,22 @@ require "./page"
 require "./chunk"
 require "./data_importer"
 
-POSITION = {
+page_schema = {
   :line_name => 3,
   :day_types => [5, 21, 36],
   :directions => [6, 13],
-  :route_types => [8, 10]
+  :route_types => [8, 10],
+  :departure_positions => {
+    :radni => 7,
+    :subota => 23,
+    :nedjelja => 38 
+  }
 }
 URL = "http://www.samoborcek.hr/linija.php?id=18" 
 
 page = Page.new(URL)
 importer = DataImporter.new(
-  :line_definition => {
-    :radni => 7,
-    :subota => 23,
-    :nedjelja => 38 
-  },
+  :page_schema => page_schema,
   :page => page
 )
 importer.import_all
