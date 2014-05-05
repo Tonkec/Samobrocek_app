a9f2ea18583d7c6d81c06829a5c76a56159cfc18
antonija <antonija1023@gmail.com>
2014-05-05 22:56:18 +0200

    aligning arrows

--- public/javascripts.js
+++ public/javascripts.js
@@ -1,9 +1,8 @@
   $(document).ready(function(){
   	  $( ".right" ).click(function() {
-  	 // $(".col-arr").append("<img class='img-bus' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
   		$(".img-bus").remove();
       $(".col-arr").append(" <img class='img-bus' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
-      $( ".img-bus" ).animate({ "left": "+=400px" }, "slow" );
+      $( ".img-bus" ).animate({ "left": "+=370px" }, "slow" );
   		$(".img-bus").fadeOut();
   		$(".col-arr").append(" <img class='img-bus' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
 
