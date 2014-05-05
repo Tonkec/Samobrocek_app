56db5880f7065a9ccda233fb50a40e587bfffad6
antonija <antonija1023@gmail.com>
2014-05-05 22:41:06 +0200

    adding bus animation

---
+++ public/javascripts.js
@@ -0,0 +1,20 @@
+  $(document).ready(function(){
+  	  $( ".right" ).click(function() {
+  	 // $(".col-arr").append("<img class='img-bus' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
+  		$(".img-bus").remove();
+      $(".col-arr").append(" <img class='img-bus' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
+      $( ".img-bus" ).animate({ "left": "+=400px" }, "slow" );
+  		$(".img-bus").fadeOut();
+  		$(".col-arr").append(" <img class='img-bus' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
+
+});
+
+
+	$( ".left" ).click(function(){
+    $(".img-bus").remove();
+		$(".col-arr").append(" <img class='img-bus' src='http://imagehost3.online-image-editor.com/images/5222935m8Q5U33Iy5/5222953GudPeMWa.png'>");
+		$(".img-bus").animate({ "left": "-=400px" }, "slow" );
+		$(".img-bus").fadeOut();
+	$(".col-arr").append("<img class='img-bus' src='http://imagehost3.online-image-editor.com/images/5222935m8Q5U33Iy5/5222953GudPeMWa.png'>");
+});
+    });
