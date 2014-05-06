df09d9606556766fc1bae614b70e2eeedac1e6ac
antonija <antonija1023@gmail.com>
2014-05-06 14:55:11 +0200

    toggling arrows

--- public/javascripts.js
+++ public/javascripts.js
@@ -1,5 +1,11 @@
   $(document).ready(function(){
-  	  $( ".right" ).click(function() {
+      $(".right").click(function() {
+      if ($("h1").length) {
+          $("h1").replaceWith("<h1> Samobor - Zagreb </h1>");
+      }
+      else {
+         $(".panel-heading").append("<h1> Samobor - Zagreb </h1>");
+       } 
   		$(".img-bus").remove();
       $(".col-arr").append(" <img class='img-bus' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
       $( ".img-bus" ).animate({ "left": "+=370px" }, "slow" );
@@ -7,11 +13,19 @@
   		$(".col-arr").append(" <img class='img-bus' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
 });
 
-	$( ".left" ).click(function(){
+	  $(".left").one("click", function() {
+    });
+    $( ".left" ).click(function(){
+        if ($("h1").length) {
+       $("h1").replaceWith("<h1> Zagreb - Samobor </h1>");
+      }
+      else {
+      $(".panel-heading").append("<p> Zagreb - Samobor </h1>");
+      }
     $(".img-bus").remove();
 		$(".col-arr").append(" <img class='img-bus img-flipped' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
 		$(".img-bus").animate({ "left": "-=400px" }, "slow" );
 		$(".img-bus").fadeOut();
-	$(".col-arr").append("<img class='img-bus img-flipped' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
+	  $(".col-arr").append("<img class='img-bus img-flipped' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
 });
     });
