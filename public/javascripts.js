43c0ce1318964a02a0fe8e318a4292947dac2279
antonija <antonija1023@gmail.com>
2014-05-06 13:29:27 +0200

    refactoring

--- public/javascripts.js
+++ public/javascripts.js
@@ -5,15 +5,13 @@
       $( ".img-bus" ).animate({ "left": "+=370px" }, "slow" );
   		$(".img-bus").fadeOut();
   		$(".col-arr").append(" <img class='img-bus' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
-
 });
-
 
 	$( ".left" ).click(function(){
     $(".img-bus").remove();
-		$(".col-arr").append(" <img class='img-bus' src='http://imagehost3.online-image-editor.com/images/5222935m8Q5U33Iy5/5222953GudPeMWa.png'>");
+		$(".col-arr").append(" <img class='img-bus img-flipped' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
 		$(".img-bus").animate({ "left": "-=400px" }, "slow" );
 		$(".img-bus").fadeOut();
-	$(".col-arr").append("<img class='img-bus' src='http://imagehost3.online-image-editor.com/images/5222935m8Q5U33Iy5/5222953GudPeMWa.png'>");
+	$(".col-arr").append("<img class='img-bus img-flipped' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
 });
     });
