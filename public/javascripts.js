4dd60c7d02b9bf52a2995b2fdfe0b9359f66d5db
antonija <antonija1023@gmail.com>
2014-05-07 15:00:52 +0200

    adjusting animation for mobile screens

--- public/javascripts.js
+++ public/javascripts.js
@@ -1,31 +1,33 @@
   $(document).ready(function(){
-      $(".icon-long-arrow-right").click(function() {
-      if ($("h1").length) {
-          $("h1").replaceWith("<h1> Samobor - Zagreb </h1>");
+    $(".icon-long-arrow-right").click(function() {
+      
+      if ($("h3").length) {
+        $("h3").replaceWith("<h3> Samobor - Zagreb </h3>");
       }
       else {
-         $(".direction").append("<h1> Samobor - Zagreb </h1>");
-       } 
+        $(".direction").append("<h3> Samobor - Zagreb </h3>");
+      }
+
   		$(".img-bus").remove();
-      $(".col-arr").append(" <img class='img-bus' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
-      $( ".img-bus" ).animate({ "left": "+=370px" }, "slow" );
+      $(".col-bus").append("<img class='img-bus' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
+      $( ".img-bus" ).animate({ "margin-left": "+=60%" }, "slow" );
   		$(".img-bus").fadeOut();
-  		$(".col-arr").append(" <img class='img-bus' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
+  		$(".col-bus").append(" <img class='img-bus' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
 });
 
-	  $(".icon-long-arrow-left").one("click", function() {
-    });
     $( ".icon-long-arrow-left" ).click(function(){
-        if ($("h1").length) {
-       $("h1").replaceWith("<h1> Zagreb - Samobor </h1>");
+      
+      if ($("h3").length) {
+        $("h3").replaceWith("<h3> Zagreb - Samobor </h3>");
       }
       else {
-      $(".direction").append("<p> Zagreb - Samobor </h1>");
+        $(".direction").append("<h3> Zagreb - Samobor </h3>");
       }
-    $(".img-bus").remove();
-		$(".col-arr").append(" <img class='img-bus img-flipped' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
-		$(".img-bus").animate({ "left": "-=390px" }, "slow" );
-		$(".img-bus").fadeOut();
-	  $(".col-arr").append("<img class='img-bus img-flipped' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
+
+      $(".img-bus").remove();
+		  $(".col-bus").append(" <img class='img-bus img-flipped' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
+		  $(".img-bus").animate({ "margin-left": "-=60%" }, "slow" );
+		  $(".img-bus").fadeOut();
+	    $(".col-bus").append("<img class='img-bus img-flipped' src='http://www.clker.com/cliparts/B/x/h/S/j/W/yellow-school-bus-hi.png'>");
 });
     });
