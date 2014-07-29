9ae4efd56f3389d211df2fdf66982035bc735808
Hrvoje Šimić <shime.ferovac@gmail.com>
2014-07-29 14:51:39 +0200

    add ability to switch destinations

--- app.rb
+++ app.rb
@@ -4,7 +4,19 @@ require 'sinatra/reloader'
 require "./bus_finder"
 
 get '/' do
-  @destination = "Zagreb"
+  redirect '/zagreb'
+end
+
+get '/zagreb' do
+  @destination = "zagreb"
+  @departure   = "samobor"
   @buses = BusFinder.execute
+  erb :index
+end
+
+get '/samobor' do
+  @destination = "samobor"
+  @departure   = "zagreb"
+  @buses = BusFinder.execute("--return")
   erb :index
 end
