c88a1a43adda52205287d53a9f073a807922b63f
Hrvoje Šimić <shime.ferovac@gmail.com>
2014-07-29 15:29:20 +0200

    DRY out app with presenter

--- app.rb
+++ app.rb
@@ -3,24 +3,34 @@ require 'sinatra/reloader'
 
 require "./bus_finder"
 
+class Presenter
+  ROUTES = {:zagreb  => :samobor,
+            :samobor => :zagreb}
+
+  attr_reader :destination, :departure,
+    :future_departures, :last_departure,
+    :day_type
+
+  def initialize(params)
+    @destination = params[:destination].to_sym
+    @departure   = ROUTES.invert[@destination]
+
+    finder_args = @destination == :samobor ? "--return" : nil
+    
+    buses = BusFinder.execute(finder_args)
+    
+    @future_departures = buses[1..-1]
+    @last_departure    = buses.first
+    @dayType = DayType.now.title
+  end
+end
+
 get '/' do
   redirect '/za/zagreb'
 end
 
-get '/za/zagreb' do
-  @destination = "zagreb"
-  @departure   = "samobor"
-  @buses = BusFinder.execute
-  @day_type = DayType.now.title
-
-  erb :index
-end
-
-get '/za/samobor' do
-  @destination = "samobor"
-  @departure   = "zagreb"
-  @buses = BusFinder.execute("--return")
-  @day_type = DayType.now.title
+get '/za/:destination' do
+  @presenter = Presenter.new(params)
 
   erb :index
 end
